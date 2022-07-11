9173a98be32935332d502e47a4be59e5951295b2
Flor <flor.zattera@gmail.com>
2022-07-10 22:40:52 -0300

    Me estoy cansando

---
+++ src/subclass/Vino.java
@@ -0,0 +1,30 @@
+package subclass;
+import superclass.CanastaNoBasica;
+import java.util.Date;
+
+public class Vino extends CanastaNoBasica {
+        private int añejamiento;
+
+        //constructor
+        public Vino(String identificador, String nombre, double costo, Date fechaDeVencimiento, boolean canastaBasica, int aniejamiento) {
+            super(identificador, nombre, costo, fechaDeVencimiento, canastaBasica);
+            this.añejamiento = aniejamiento;
+        }
+
+        public int getAñejamiento() {
+            return añejamiento;
+        }
+
+        @Override
+        public double ObtenerPrecio() {
+            double precioFinal = super.ObtenerPrecio();
+            int multiplicador = calcularMultiplicador();
+
+            return precioFinal + (precioFinal * (multiplicador / 10));
+        }
+
+        private int calcularMultiplicador() {
+            return añejamiento / 5;
+        }
+    }
+
---
+++ src/superclass/CanastaNoBasica.java
@@ -0,0 +1,18 @@
+package superclass;
+
+import java.util.Date;
+
+    public class CanastaNoBasica extends Articulo {
+        public static final double IVA = 0.18;
+
+        //constructor
+        public CanastaNoBasica(String identificador, String nombre, double costo, Date fechaDeVencimiento, boolean canastaBasica) {
+            super(identificador, nombre, costo, fechaDeVencimiento, false);
+        }
+
+        @Override
+        public double ObtenerPrecio() {
+            return (costo * GANANCIA) + (costo * IVA);
+        }
+    }
+
